64564a4aa9fceab57c95d29def62864595980755
Alex Nichol <unixpickle@gmail.com>
2016-11-04 09:46:54 -0400

    allow re-training with lower step size

--- demo/mnist/train/main.go
+++ demo/mnist/train/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/unixpickle/mnist"
 	"github.com/unixpickle/num-analysis/linalg"
@@ -14,12 +15,71 @@ import (
 	"github.com/unixpickle/weakai/rnn"
 )
 
+const (
+	DefaultStepSize = 0.001
+	BatchSize       = 16
+)
+
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "<output_net>")
+	if len(os.Args) != 2 && len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "<output_net> [step_size]")
 		os.Exit(1)
 	}
 
+	stepSize := DefaultStepSize
+	if len(os.Args) == 3 {
+		var err error
+		stepSize, err = strconv.ParseFloat(os.Args[2], 64)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Invalid step size:", err)
+			os.Exit(1)
+		}
+	}
+
+	var net neuralnet.Network
+	if netData, err := ioutil.ReadFile(os.Args[1]); err == nil {
+		net, err = neuralnet.DeserializeNetwork(netData)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to decode network:", err)
+			os.Exit(1)
+		}
+	} else {
+		log.Println("Creating network...")
+		net = createNetwork()
+	}
+
+	training := dataSetSamples(mnist.LoadTrainingDataSet())
+
+	grad := &sgd.RMSProp{
+		Gradienter: &neuralnet.BatchRGradienter{
+			Learner:       net.BatchLearner(),
+			CostFunc:      neuralnet.MeanSquaredCost{},
+			MaxBatchSize:  BatchSize,
+			MaxGoroutines: 1,
+		},
+		Resiliency: 0.9,
+	}
+
+	var iter int
+	sgd.SGDMini(grad, training, stepSize, BatchSize, func(s sgd.SampleSet) bool {
+		cost := neuralnet.TotalCost(neuralnet.MeanSquaredCost{}, net, s)
+		log.Printf("iter %d: cost=%f", iter, cost)
+		iter++
+		return true
+	})
+
+	data, err := net.Serialize()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to serialize output:", err)
+		os.Exit(1)
+	}
+	if err := ioutil.WriteFile(os.Args[1], data, 0755); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to write output:", err)
+		os.Exit(1)
+	}
+}
+
+func createNetwork() neuralnet.Network {
 	net := neuralnet.Network{
 		&neuralnet.DenseLayer{
 			InputCount:  28 * 28,
@@ -41,33 +101,7 @@ func main() {
 		neuralnet.Sigmoid{},
 	}
 	net.Randomize()
-	training := dataSetSamples(mnist.LoadTrainingDataSet())
-
-	grad := &sgd.RMSProp{
-		Gradienter: &neuralnet.BatchRGradienter{
-			Learner:  net.BatchLearner(),
-			CostFunc: neuralnet.MeanSquaredCost{},
-		},
-		Resiliency: 0.9,
-	}
-
-	var iter int
-	sgd.SGDMini(grad, training, 0.001, 16, func(s sgd.SampleSet) bool {
-		cost := neuralnet.TotalCost(neuralnet.MeanSquaredCost{}, net, s)
-		log.Printf("iter %d: cost=%f", iter, cost)
-		iter++
-		return true
-	})
-
-	data, err := net.Serialize()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to serialize output:", err)
-		os.Exit(1)
-	}
-	if err := ioutil.WriteFile(os.Args[1], data, 0755); err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to write output:", err)
-		os.Exit(1)
-	}
+	return net
 }
 
 func dataSetSamples(d mnist.DataSet) sgd.SampleSet {
